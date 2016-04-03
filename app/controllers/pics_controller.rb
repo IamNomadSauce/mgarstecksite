cbdcffd189a7ce257019ee7d92af91abe9a865c6
mgarsteck <g-photography@hotmail.com>
2016-04-03 00:03:44 -0700

    Connect pictures to portfolio page

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,19 +1,35 @@
 class PicsController < ApplicationController
-
+  before_action :find_pic, only: [:show, :edit, :update, :destroy]
   def index
+    @pics = Pic.all.order("created_at DESC")
   end
 
   def new
     @pic = Pic.new
   end
 
+  def show
+
+  end
+
   def create
     @pic = Pic.new(pic_params)
+
+    if @pic.save
+      flash[:success] = "The picture was posted properly"
+      redirect_to pic_path(@pic)
+    else
+      redirect_to :new
+    end
   end
 
   private
 
   def pic_params
     params.require(:pic).permit(:title, :description)
+  end
+
+  def find_pic
+    @pic = Pic.find(params[:id])
   end
 end
