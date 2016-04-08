d1de9f3b920d87c3c792322fe6d0557af70a8497
mgarsteck <g-photography@hotmail.com>
2016-04-07 23:22:20 -0700

    Fixed bugs with the migrations, started building the clients pages and functionality, styling

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,7 @@ Rails.application.routes.draw do
   resources :pics
   get 'pages/control'
 
-  root 'pages#index'
-
+  root 'pages#control'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
